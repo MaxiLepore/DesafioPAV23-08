cd90e62aaa93e6710180f38c080246ea5247c20b
leporemaxi95 <leporemaxi95@gmail.com>
2021-08-22 22:45:44 -0300

    AgregoProyectoPunto1

---
+++ PruebaLogin/PruebaLogin/InterfacesDeUsuario/FrmLogin.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace PruebaLogin
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtLoginUser = new System.Windows.Forms.TextBox();
+            this.TxtLoginPass = new System.Windows.Forms.TextBox();
+            this.LblLoginUser = new System.Windows.Forms.Label();
+            this.LblLoginPass = new System.Windows.Forms.Label();
+            this.BtnIngresar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TxtLoginUser
+            // 
+            this.TxtLoginUser.Location = new System.Drawing.Point(105, 30);
+            this.TxtLoginUser.Name = "TxtLoginUser";
+            this.TxtLoginUser.Size = new System.Drawing.Size(157, 20);
+            this.TxtLoginUser.TabIndex = 1;
+            // 
+            // TxtLoginPass
+            // 
+            this.TxtLoginPass.Location = new System.Drawing.Point(105, 82);
+            this.TxtLoginPass.Name = "TxtLoginPass";
+            this.TxtLoginPass.Size = new System.Drawing.Size(157, 20);
+            this.TxtLoginPass.TabIndex = 2;
+            this.TxtLoginPass.UseSystemPasswordChar = true;
+            // 
+            // LblLoginUser
+            // 
+            this.LblLoginUser.AccessibleName = "";
+            this.LblLoginUser.AutoSize = true;
+            this.LblLoginUser.Location = new System.Drawing.Point(51, 30);
+            this.LblLoginUser.Name = "LblLoginUser";
+            this.LblLoginUser.Size = new System.Drawing.Size(46, 13);
+            this.LblLoginUser.TabIndex = 3;
+            this.LblLoginUser.Text = "Usuario:";
+            this.LblLoginUser.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // LblLoginPass
+            // 
+            this.LblLoginPass.AutoSize = true;
+            this.LblLoginPass.Location = new System.Drawing.Point(41, 82);
+            this.LblLoginPass.Name = "LblLoginPass";
+            this.LblLoginPass.Size = new System.Drawing.Size(64, 13);
+            this.LblLoginPass.TabIndex = 4;
+            this.LblLoginPass.Text = "Contraseña:";
+            // 
+            // BtnIngresar
+            // 
+            this.BtnIngresar.Location = new System.Drawing.Point(113, 119);
+            this.BtnIngresar.Name = "BtnIngresar";
+            this.BtnIngresar.Size = new System.Drawing.Size(87, 35);
+            this.BtnIngresar.TabIndex = 5;
+            this.BtnIngresar.Text = "Ingresar";
+            this.BtnIngresar.UseVisualStyleBackColor = true;
+            this.BtnIngresar.Click += new System.EventHandler(this.BtnIngresar_Click);
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 166);
+            this.Controls.Add(this.BtnIngresar);
+            this.Controls.Add(this.LblLoginPass);
+            this.Controls.Add(this.LblLoginUser);
+            this.Controls.Add(this.TxtLoginPass);
+            this.Controls.Add(this.TxtLoginUser);
+            this.Name = "FrmLogin";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtLoginUser;
+        private System.Windows.Forms.TextBox TxtLoginPass;
+        private System.Windows.Forms.Label LblLoginUser;
+        private System.Windows.Forms.Label LblLoginPass;
+        private System.Windows.Forms.Button BtnIngresar;
+    }
+}
+
---
+++ PruebaLogin/PruebaLogin/InterfacesDeUsuario/FrmLogin.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PruebaLogin
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnIngresar_Click(object sender, EventArgs e)
+        {
+            // Declaración de variables
+            string userPermitido; // Declaración de variable
+            userPermitido = "admin"; // Asignación de variable
+            string passPermitida = "admin"; // Declaración y asignación juntos
+
+            // Obtener los valores de los textbox (string -> cadena de texto con "" / var -> toma el tipo del dato del primer valor que lo define)
+            // .Trim() saca los espacios al principio y final del string para validar estos tipos de errores de los usuarios.
+            var userIngresado = TxtLoginUser.Text.Trim();
+            var passIngresado = TxtLoginPass.Text.Trim();
+
+           //Estructura de control if para comparar el user ingresado con el definido como válido
+           if(userIngresado == userPermitido && passIngresado == passPermitida)            
+           {
+                MessageBox.Show("Hola admin", "Bienvenido admin", MessageBoxButtons.OK );
+           }
+           else
+           {
+                MessageBox.Show("Usuario y/o contraseña incorrecta", "Bienvenido admin", MessageBoxButtons.OK);
+           }
+
+        }
+    }
+}
